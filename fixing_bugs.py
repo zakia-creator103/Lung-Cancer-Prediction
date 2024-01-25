1d910a3f72f0c8525c2569607c64a3b3cc8dd560
zakia rana <zakiarana103@gmail.com>
2024-01-25 20:59:52 +0500

    fixing bugs

---
+++ fixing_bugs.py
@@ -0,0 +1,8 @@
+# lung_cancer_prediction_bug_fix.py
+
+def main():
+    # Fixed bug: Corrected print statement
+    print("Lung Cancer Prediction - Main Branch (Bug Fix)")
+
+if __name__ == "__main__":
+    main()
